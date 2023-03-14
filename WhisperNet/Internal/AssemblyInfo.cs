beb61fb3082692653ae8891e4ebf61184529b53f
Konstantin <const@const.me>
2023-03-14 20:44:46 +0100

    Version 1.9

--- WhisperNet/Internal/AssemblyInfo.cs
+++ WhisperNet/Internal/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright( "Copyright © const.me, 2022-2023" )]
 [assembly: ComVisible( false )]
 [assembly: Guid( "ced6cdb7-e040-4398-bae8-3417e5fa35f1" )]
-[assembly: AssemblyVersion( "1.8.0.0" )]
+[assembly: AssemblyVersion( "1.9.0.0" )]
 [assembly: AssemblyDescription( "DirectCompute port of whisper.cpp library, C# bindings" )]
