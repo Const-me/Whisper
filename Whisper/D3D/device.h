ca0e1272d67d7eb4290eea5e25ee15a79a14fe72
Konstantin <const@const.me>
2023-03-17 19:03:46 +0100

    Refactor, moved a structure

--- Whisper/D3D/device.h
+++ Whisper/D3D/device.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <atlcomcli.h>
 #include <string>
+#include "sGpuInfo.h"
 
 namespace DirectCompute
 {
@@ -11,45 +12,6 @@ namespace DirectCompute
 	HRESULT initialize( uint32_t flags, const std::wstring& adapter );
 	void terminate();
 
-	// DXGI_ADAPTER_DESC.VendorId magic numbers; they come from that database: https://pcisig.com/membership/member-companies
-	enum struct eGpuVendor : uint16_t
-	{
-		AMD = 0x1002,
-		NVidia = 0x10de,
-		Intel = 0x8086,
-		VMWare = 0x15ad,
-	};
-
-	enum struct eGpuEffectiveFlags : uint8_t
-	{
-		Wave64 = 1,
-		ReshapedMatMul = 2,
-	};
-
-	struct sGpuInfo
-	{
-		eGpuEffectiveFlags flags;
-		eGpuVendor vendor;
-		uint16_t device, revision;
-		uint32_t subsystem;
-		size_t vramDedicated, ramDedicated, ramShared;
-		std::wstring description;
-
-		inline bool wave64() const
-		{
-			return 0 != ( (uint8_t)flags & (uint8_t)eGpuEffectiveFlags::Wave64 );
-		}
-
-		// On nVidia 1080Ti that approach is much slower, by a factor of 2.4
-		// On AMD Cezanne that approach is faster by a factor of 0.69, i.e. 30% faster.
-		// Dunno why that is, maybe 'coz on that AMD complete panels fit in L3 cache.
-		// Anyway, we do want extra 30% perf on AMD Cezanne, so only using that code on AMD GPUs.
-		// Dunno how it gonna behave on other GPUs, need to test.
-		inline bool useReshapedMatMul() const
-		{
-			return 0 != ( (uint8_t)flags & (uint8_t)eGpuEffectiveFlags::ReshapedMatMul );
-		}
-	};
 	const sGpuInfo& gpuInfo();
 
 	inline bool available()
