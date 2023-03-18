f23343131aabd00527d00a5d6a764079603d84af
Konstantin <const@const.me>
2023-03-18 16:04:55 +0100

    Release automation

--- Tools/copy-binaries.cmd
+++ Tools/copy-binaries.cmd
@@ -22,3 +22,22 @@ rem C++ CLI example
 mkdir ..\..\Release\Next\cli
 copy ..\x64\Release\Whisper.dll ..\..\Release\Next\cli\
 copy ..\x64\Release\main.exe ..\..\Release\Next\cli\
+
+rem PowerShell module
+mkdir ..\..\Release\Next\WhisperPS
+copy ..\Examples\WhisperPS\bin\Release\*.dll ..\..\Release\Next\WhisperPS\
+copy ..\Examples\WhisperPS\bin\Release\WhisperPS.psd1 ..\..\Release\Next\WhisperPS\
+copy ..\Examples\WhisperPS\bin\Release\WhisperPS.dll.config ..\..\Release\Next\WhisperPS\
+copy ..\Examples\WhisperPS\bin\Release\WhisperPS.dll-Help.xml ..\..\Release\Next\WhisperPS\
+
+rem Zip these things, using command-line 7zip.exe with maximum level
+rem https://www.7-zip.org/download.html
+
+set ZIP="C:\Program Files\7-Zip\7z.exe" a -mmt1 -mx9 -bd
+pushd ..\..\Release\Next
+%ZIP% -tzip .\WhisperDesktop.zip .\WhisperDesktop\*.*
+%ZIP% -tzip -r .\Library.zip .\Library\Include .\Library\Linker .\Library\Binary
+%ZIP% -t7z -sdel .\Whisper.pdb.7z .\Whisper.pdb
+%ZIP% -tzip .\cli.zip .\cli\*.*
+%ZIP% -tzip -r .\WhisperPS.zip .\WhisperPS
+popd
