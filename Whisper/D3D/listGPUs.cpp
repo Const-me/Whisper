34dca8c951e1b6382c79754bd595eab022f33ab9
Konstantin <const@const.me>
2023-04-02 12:17:15 +0200

    Library can now select GPUs with 0-based index, in addition to their names
    Some people have multiple GPUs of the same model, see issue #72

--- Whisper/D3D/listGPUs.cpp
+++ Whisper/D3D/listGPUs.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "listGPUs.h"
 #pragma comment(lib, "DXGI.lib")
+#include <charconv>
+#include <optional>
 
 namespace DirectCompute
 {
@@ -19,6 +21,30 @@ namespace DirectCompute
 		rdi.assign( rsi, rsi + descLen );
 	}
 
+	// If the UTF16 string contains a small non-negative number, return that number.
+	std::optional<uint32_t> parseGpuIndex( const std::wstring& requestedName )
+	{
+		if( requestedName.length() > 3 )
+			return {};
+
+		char buffer[ 4 ];
+		*(uint32_t*)( &buffer[ 0 ] ) = 0;
+		for( size_t i = 0; i < requestedName.length(); i++ )
+		{
+			const wchar_t wc = requestedName[ i ];
+			if( wc < L'0' || wc > L'9' )
+				return {};
+			buffer[ i ] = (char)(uint8_t)wc;
+		}
+
+		uint32_t result;
+		auto res = std::from_chars( buffer, &buffer[ 0 ] + requestedName.length(), result );
+		if( res.ec == std::errc{} )
+			return result;
+
+		return {};
+	}
+
 	CComPtr<IDXGIAdapter1> selectAdapter( const std::wstring& requestedName )
 	{
 		if( requestedName.empty() )
@@ -30,6 +56,29 @@ namespace DirectCompute
 		{
 			logWarningHr( hr, u8"CreateDXGIFactory1 failed" );
 			return nullptr;
+		}
+
+		const auto idx = parseGpuIndex( requestedName );
+		if( idx.has_value() )
+		{
+			// User has specified 0-based GPU index instead of the name
+			// https://github.com/Const-me/Whisper/issues/72
+
+			CComPtr<IDXGIAdapter1> adapter;
+			hr = dxgi->EnumAdapters1( idx.value(), &adapter );
+			if( hr == DXGI_ERROR_NOT_FOUND )
+			{
+				logWarning( u8"Requested GPU #%i not found", (int)idx.value() );
+				return nullptr;
+			}
+
+			if( FAILED( hr ) )
+			{
+				logWarningHr( hr, u8"IDXGIFactory1.EnumAdapters1 failed" );
+				return nullptr;
+			}
+
+			return adapter;
 		}
 
 		std::wstring name;
@@ -45,7 +94,7 @@ namespace DirectCompute
 
 			if( FAILED( hr ) )
 			{
-				logErrorHr( hr, u8"IDXGIFactory1.EnumAdapters1 failed" );
+				logWarningHr( hr, u8"IDXGIFactory1.EnumAdapters1 failed" );
 				return nullptr;
 			}
 
